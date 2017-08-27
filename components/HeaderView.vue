454c68b5a95a9b55c6756fbc359ae1520d734856
dnomak <me@dnomak.com>
2017-08-27 13:33:16 +0300

    header düzenlendi

--- components/HeaderView.vue
+++ components/HeaderView.vue
@@ -1,17 +1,17 @@
 <template lang="pug">
-  .xl-tal.xl-py24.xl-px24.xl-ba-black-500.xl-co-white
+  .xl-tal.xl-p8.xl-px24.xl-ba-black-500.xl-co-white
     .xl-maw1280.xl-ma
-      .wrap.xl-auto.xl-middle.xl-left.md-1.xl-gutter-16.md-center
+      .wrap.xl-auto.xl-middle.xl-left.xl-gutter-16
         .col
-          nuxt-link.xl-h40.xl-lh40.xl-vam.xl-dib.xl-fs24.xl-co-black-200.xl-fw700(to="/") dnomak.com
+          nuxt-link.xl-h56.xl-lh56.xl-vam.xl-dibxl-fs14.xl-co-gray-500.xl-fw700(to="/") DNOMAK.COM
         template(v-if="$route.name === 'svg-turkiye-haritasi'")
-          .col.md-hidden
+          .col
             .xl-w4.xl-h40.xl-br4.xl-ba-white
           .col.xl-tac
-            a.xl-h40.xl-lh40.xl-vam.xl-dib.xl-fs30.xl-co-white.md-mt16(href="https://github.com/dnomak/svg-turkiye-haritasi", target="_blank") SVG Türkiye Haritası
+            a.xl-h56.xl-lh56.xl-vam.xl-dib.xl-fs24.xl-co-white(href="https://github.com/dnomak/svg-turkiye-haritasi", target="_blank") SVG Türkiye Haritası
         template(v-if="$route.params.username")
-          .col.md-hidden
+          .col
             .xl-w4.xl-h40.xl-br4.xl-ba-white
           .col.xl-tac
-            a.xl-h40.xl-lh40.xl-vam.xl-dib.xl-fs30.xl-co-white.md-mt16(href="https://youtube.com/dnomak", target="_blank") YouTube
+            a.xl-h56.xl-lh56.xl-vam.xl-dib.xl-fs24.xl-co-white(href="https://youtube.com/dnomak", target="_blank") YouTube
 </template>
