cddcac0aeb71527d6c2298857cde24288f6b5d99
dnomak <me@dnomak.com>
2017-11-05 23:59:07 +0300

    version update

--- pages/udemy.vue
+++ pages/udemy.vue
@@ -25,7 +25,7 @@
       }
     },
     mounted () {
-      window.location.href = 'https://www.udemy.com/web-gelistiricisi-olmann-temelleri/?couponCode=5NX47A'
+      window.location.href = 'https://www.udemy.com/web-gelistiricisi-olmann-temelleri/?couponCode=DNOMAK'
     }
   }
 </script>
