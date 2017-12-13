0d35ee0cc21e29b5f9ed7d436fc4ce776594260e
dnomak <me@dnomak.com>
2017-12-13 11:10:48 +0300

    update

--- static/js/one-signal.js
+++ static/js/one-signal.js
@@ -3,6 +3,6 @@ OneSignal.push(["init", {
   appId: "a8c04752-7525-4948-a678-8b6b2b95ee9c",
   autoRegister: true, /* Set to true to automatically prompt visitors */
   notifyButton: {
-    enable: false /* Set to false to hide */
+    enable: true /* Set to false to hide */
   }
 }]);
