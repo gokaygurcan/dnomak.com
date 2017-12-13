8a34a627984fafbff60a9d1b37b886a3938bd481
dnomak <me@dnomak.com>
2017-12-13 10:55:31 +0300

    one signal config

--- static/js/one-signal.js
+++ static/js/one-signal.js
@@ -1,8 +1,8 @@
 var OneSignal = window.OneSignal || [];
 OneSignal.push(["init", {
   appId: "9f0c4e15-58a4-4984-bae3-1f7de7ed44a4",
-  autoRegister: false, /* Set to true to automatically prompt visitors */
+  autoRegister: true, /* Set to true to automatically prompt visitors */
   notifyButton: {
-      enable: true /* Set to false to hide */
+    enable: false /* Set to false to hide */
   }
 }]);
